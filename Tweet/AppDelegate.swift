388a909bdb3ff721170dcd09de28710edabd11ba
John <johnkenny1985@gamil.com>
2017-09-18 16:31:24 +0100

    Initial Commit

---
+++ Tweet/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tweet
+//
+//  Created by JOHN KENNY on 18/09/2017.
+//  Copyright © 2017 JOHN KENNY. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tweet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tweet
+//
+//  Created by JOHN KENNY on 18/09/2017.
+//  Copyright © 2017 JOHN KENNY. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
